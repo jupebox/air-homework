2e5bd7857fd5c5a1348e41be12dfcdbc8e12281a
Jupebox <linniejupe@gmail.com>
2026-08-07 13:09:08 -0700

    fix: Accessibility pass over ToggleSection

--- src/app/components/ToggleSection/ToggleSection.tsx
+++ src/app/components/ToggleSection/ToggleSection.tsx
@@ -14,7 +14,7 @@ export const ToggleSection = ({children, id, itemQuantity, initialIsExpanded, na
     return (
         <section id={id} className={styles.section}>
             {isExpandable ? <h2 className={styles.heading}>
-                <button className={styles.button} type="button" aria-expanded={isExpanded ? "true" : "false"} aria-controls={`${id}-content`} onClick={toggleIsExpanded}>{name} ({itemQuantity}) <span aria-hidden className={`${styles.arrow} ${isExpanded ? styles.expanded : ''}`}><Arrow /></span></button>
+                <button className={styles.button} type="button" aria-expanded={isExpanded ? "true" : "false"} aria-controls={`${id}-content`} onClick={toggleIsExpanded}>{name} ({itemQuantity}) <span aria-hidden className={styles.arrow}><Arrow /></span></button>
             </h2>
             : 
             <h2 className={`${styles.heading} ${styles.notExpandable}`}>{name} ({itemQuantity})</h2>}
@@ -23,8 +23,8 @@ export const ToggleSection = ({children, id, itemQuantity, initialIsExpanded, na
                     {hasLoaded ? <p>This section is empty. Try adding {name.toLocaleLowerCase()} to it!</p> : <p>Loading...</p>}
                 </div>
             ) :
-            (<div id={`${id}-content`}>
-                {isExpanded ? children : ''}
+            (<div id={`${id}-content`} hidden={!isExpanded}>
+                {children}
             </div>)}
             
         </section>
--- src/app/components/icons/Arrow/Arrow.tsx
+++ src/app/components/icons/Arrow/Arrow.tsx
@@ -1,6 +1,7 @@
+// ideally we would define this once and be able to <use> it later but I'm considering that out of scope for now
 export const Arrow = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
+        <svg focusable="false" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
             <path fillRule="evenodd" d="M8 11.5L3.5 7h9L8 11.5z"/>
         </svg>
     )
