99b0a8f2ce2dab52128c3bdb94d7251c574fc833
Jupebox <linniejupe@gmail.com>
2026-08-07 14:55:47 -0700

    fix: Prevent overlay from escaping short cards

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -24,7 +24,7 @@ export const Card = ({ href, title, titleStyle = 'always', thumbnail, height, wi
         <li className={styles.card} style={ height && width ? cardStyle : undefined}>
             {/* if I had time to make the other pages, this would be a <Link> */}
             {/* would need further direction on how assets and boards are organized and linked together to create dynamic pages with confidence */}
-            <a className={`${styles.title} ${styles[titleStyle]}`} href={href}>{title}</a>
+            <a className={`${styles.title} ${styles[titleStyle]}`} href={href}><span>{title}</span></a>
             {thumbnail ? <img alt="" className={styles.image} src={thumbnail} /> : ''}
         </li>
     );
